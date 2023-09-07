8381732d723315f1b4d4113ee1c68fb1b9011f2d
ramy-suly <rsomas22@gmail.com>
2023-09-07 16:00:36 -0700

    regulaer

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - Entry Point
+ *
+ * Description - a C program print with puts FUN.
+ *
+ * Return - Alwayes 0
+*/
+
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+		return (0);
+}
