4a9c377ed341a3066bdf1e56da51705893748c20
ramy-suly <rsomas22@gmail.com>
2023-09-07 16:43:31 -0700

    regulaer

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,19 @@
+#include <sstdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: A c program that count a size of byte for var
+ *
+ * Return: Always 0 (success)
+*/
+
+int main(void)
+{
+	printf("size of a char: %lu byte(s) \n" .sizeof(char));
+	printf("size of an int: %lu byte(s) \n" .sizeof(int));
+	printf("size of a long int: %lu byte(s) \n" .sizeof(long int));
+	printf("size of along long int: %lu byte(s) \n" .sizeof(long long int));
+	printf("size of a floot: %lu byte(s) \n" .sizeof(float));
+	return (0);
+}
