979fb4dac4d01906214391c709cff743b71155af
ramy-suly <rsomas22@gmail.com>
2023-09-07 16:16:17 -0700

    regulaer

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,15 +1,15 @@
 #include <stdio.h>
 
 /**
- * main - Entry Point
+ * main - entry point
  *
- * Description - a C program print with puts FUN.
+ * Description: A c program that type with funcction puts
  *
- * Return - Alwayes 0
+ * Return: Always 0 (success)
 */
 
-int main(void)
+init main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
-		return (0);
+	return (0);
 }
