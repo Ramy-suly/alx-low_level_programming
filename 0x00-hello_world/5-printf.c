ef7da8ad02460576f49182897b10eaf6067598bf
ramy-suly <rsomas22@gmail.com>
2023-09-07 16:23:56 -0700

    regulaer

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ *
+ * Description: a c program taht print wit func. printf
+ *
+ * Return: Always 0 (success)
+ */
+
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art, \n");
+		return (0);
+}
