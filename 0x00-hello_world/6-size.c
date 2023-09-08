7d47f33c9ea1fd734a685a01e42bb61e85883e8a
ramy-suly <rsomas22@gmail.com>
2023-09-07 17:21:13 -0700

    regulaer

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,19 +1,19 @@
 #include <stdio.h>
 
 /**
- * main - Entry point
+ * main - entry point
  *
- * Description: A c program that count a size of byte for var
+ * Description: c program to count a size of var type
  *
- * Return: Always 0 (success)
+ * Return: Always0 (success)
 */
 
 int main(void)
 {
-        printf("Size of a char: %u byte(s)\n", sizeof(char));
-        printf("Size of an int: %u byte(s)\n", sizeof(int));
-        printf("Size of a long int: %u byte(s)\n", sizeof(long int));
-        printf("Size of a long long int: %u byte(s)\n", sizeof(long long int));
-        printf("Size of a floot: %u byte(s)\n", sizeof(float));
-        return (0);
+	printf("Size of a char: %u byte(s)\n", sizeof(char));
+	printf("Size of an int: %u byte(s)\n", sizeof(int));
+	printf("Size of a long int: %u byte(s)\n", sizeof(long int));
+	printf("Size of a long long int: %u byte(s)\n", sizeof(long long int));
+	printf("Size of a float: %u byte(s)\n", sizeof(float));
+	return (0);
 }
