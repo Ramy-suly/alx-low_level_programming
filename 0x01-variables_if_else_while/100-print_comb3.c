07852ff0a6853e7b1eaa170bc312d2b1578e24e4
ramy-suly <rsomas22@gmail.com>
2023-09-09 19:10:41 -0700

    last digit

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -1,37 +1,30 @@
 #include <stdio.h>
 
 /**
- * main - entry point
+ * main - Prints all possible combinations of two different digits,
+ *        in ascending order, separated by a comma followed by a space.
  *
- * Description: Print all alphapet letters
- *
- * Return: 0(success)
-*/
-
+ * Return: Always 0.
+ */
 int main(void)
 {
-	int digit1 = 0, digit2;
+	int digit1, digit2;
 
-	while (digit1 <= 9)
+	for (digit1 = 0; digit1 < 9; digit1++)
 	{
-		digit2 = 0;
-		while (digit2 <= 9)
+		for (digit2 = digit1 + 1; digit2 < 10; digit2++)
 		{
-			if (digit1 != digit2 && digit1 < digit2)
-			{
-				putchar(digit1 + 48);
-				putchar(digit2 + 48);
+			putchar((digit1 % 10) + '0');
+			putchar((digit2 % 10) + '0');
 
-				if (digit1 != 17)
-				{
-					putchar(',');
-					putchar(' ');
-				}
-			}
-			digit2++;
+			if (digit1 == 8 && digit2 == 9)
+				continue;
+
+			putchar(',');
+			putchar(' ');
 		}
-		digit1++;
 	}
+
 	putchar('\n');
 
 	return (0);
