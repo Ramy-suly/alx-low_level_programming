ad55b47b42b9ea939d4575122a2bfc1ceca6fbb1
ramy-suly <rsomas22@gmail.com>
2023-09-12 15:04:20 -0700

    my commit

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,20 +1,20 @@
 #include "main.h"
 
-/*
- * main - Entry point
+/**
+ * main - Prints "_putchar" followed by a new line.
  *
- * Description: print _putchar eithout printf ot puts
- *
- * Return: Always 0 (success)
-*/
-
+ * Return: Always 0.
+ */
 int main(void)
 {
-	char str[] = "_putchar";
-	int ch;
-
-	for (ch = 0; ch < 8; ch++)
-		_putchar(str[ch]);
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
 	_putchar('\n');
 
 	return (0);
