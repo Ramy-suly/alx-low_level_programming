3bd1f64a8ac3e3269fc3b6a2a389a71f384a643f
ramy-suly <rsomas22@gmail.com>
2023-09-18 09:23:30 -0700

    my commit

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -10,13 +10,12 @@
  */
 char *_strcpy(char *dest, const char *src)
 {
-	int i = -i;
+	int i = -1;
 
 	do {
 		i++;
 		dest[i] = src[i];
-	}
-	while (src[i] != '\0');
+	}while (src[i] != '\0');
 
 	return (dest);
 }
