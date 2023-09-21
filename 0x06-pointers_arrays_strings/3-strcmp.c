faa22eb87918b90c0ec1defb765658df859b01d0
ramy-suly <rsomas22@gmail.com>
2023-09-21 09:04:39 -0700

    comit

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -5,18 +5,24 @@
  * @s1: A pointer to the first string to be compared.
  * @s2: A pointer to the second string to be compared.
  *
- * Return: If str1 < str2, the negative difference of the first unmatched characters.
+ * Return: If str1 < str2 the negative difference of first unmatched characters
  *         If str1 == str2, 0.
- *         If str1 > str2, the positive difference of the first unmatched characters.
+ *         If str1 > str2,the positive difference of first unmatched characters
  */
 
 int _strcmp(char *s1, char *s2)
 {
-	while (*s1 && *s2 && *s1 == *s2)
+	int equal = 0;
+
+	while (*s1)
 	{
+		if (*s1 != *s2)
+		{
+			equal = ((int)*s1 - 48) - ((int)*s2 - 48);
+			break;
+		}
 		s1++;
 		s2++;
 	}
-
-	return (*s1 - *s2);
+	return (equal);
 }
