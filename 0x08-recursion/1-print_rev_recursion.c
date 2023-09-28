cbeb83149d74927471d2591d27e9c29a2657e7ef
ramy-suly <rsomas22@gmail.com>
2023-09-28 13:52:16 -0700

    my commite

---
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -0,0 +1,16 @@
+#include "mmain.h"
+
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+void  _print_rev_recursion(char *s)
+{
+    if (*s)
+    {
+        _print_rev_recursion(s + 1);
+        printf("%c", *s);
+
+    }
+}
