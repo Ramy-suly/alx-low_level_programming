c5a417708fc87bef37d50f0de3c2a1e7442387a4
ramy-suly <rsomas22@gmail.com>
2023-09-28 13:55:24 -0700

    my commite

--- 0x08-recursion/1-print_rev_recursion.c
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -1,16 +1,14 @@
 #include "main.h"
 
 /**
- * main - check the code
- *
- * Return: Always 0.
+ * _print_rev_recursion - Prints a string in reverse.
+ * @s: The string to be printed.
  */
-void  _print_rev_recursion(char *s)
+void _print_rev_recursion(char *s)
 {
-    if (*s)
-    {
-        _print_rev_recursion(s + 1);
-        printf("%c", *s);
-
-    }
+	if (*s)
+	{
+		_print_rev_recursion(s + 1);
+		_putchar(*s);
+	}
 }
